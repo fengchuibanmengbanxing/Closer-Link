02915d7dbf6eb4b4487be6a3f6bb49219706df1b
风吹半梦半醒 <2422016922@qq.com>
2024-05-14 21:17:47 +0800

    未序列化包装类

---
+++ admin/src/main/java/com_awake_CloserLink/Dto/Respons/UserActualRespDTO.java
@@ -0,0 +1,41 @@
+package com_awake_CloserLink.Dto.Respons;
+
+import lombok.Data;
+
+/**
+ * @Author 清醒
+ * @Date 2024/5/14 20:00
+ */
+
+
+/**
+ * 用户响应类*/
+@Data
+public class UserActualRespDTO {
+    /**
+     * ID
+     */
+    private String id;
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+
+    /**
+     * 真实姓名
+     */
+    private String realName;
+
+    /**
+     * 手机号
+     */
+
+    private String phone;
+
+    /**
+     * 邮箱
+     */
+    private String mail;
+}
