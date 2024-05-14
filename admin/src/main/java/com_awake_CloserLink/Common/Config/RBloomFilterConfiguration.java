76e61d2fdafef0266ffcf1407085534d3addf9b4
风吹半梦半醒 <2422016922@qq.com>
2024-05-14 23:08:02 +0800

    布隆过滤器配置

---
+++ admin/src/main/java/com_awake_CloserLink/Common/Config/RBloomFilterConfiguration.java
@@ -0,0 +1,28 @@
+package com_awake_CloserLink.Common.Config;
+import org.redisson.api.RBloomFilter;
+import org.redisson.api.RedissonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author 清醒
+ * @Date 2024/5/14 21:40
+ */
+/**
+ * 布隆过滤器配置
+ */
+@Configuration
+public class RBloomFilterConfiguration {
+
+    /**
+     * 防止用户注册查询数据库的布隆过滤器
+     */
+    @Bean
+    public RBloomFilter<String> userRegisterCachePenetrationBloomFilter(RedissonClient redissonClient) {
+        RBloomFilter<String> cachePenetrationBloomFilter = redissonClient.getBloomFilter("xxx");
+        cachePenetrationBloomFilter.tryInit(0, 0);
+        return cachePenetrationBloomFilter;
+    }
+
+
+}
