106401d6786336affb3c3d17d1541629b6121760
风吹半梦半醒 <2422016922@qq.com>
2024-05-15 17:28:54 +0800

    mybatis-plus 字段自动填充

--- admin/src/main/java/com_awake_CloserLink/Common/Config/MyMetaObjectHandler.java
+++ admin/src/main/java/com_awake_CloserLink/Common/Config/MyMetaObjectHandler.java
@@ -11,6 +11,8 @@ import java.util.Date;
 /**
  * @Author 清醒
  * @Date 2024/5/14 22:54
+ * <p>
+ * 字段自动填充
  */
 @Slf4j
 @Component
@@ -20,22 +22,19 @@ public class MyMetaObjectHandler implements MetaObjectHandler {
     @Override
     public void insertFill(MetaObject metaObject) {
         log.info("start insert fill ....");
-        this.strictInsertFill(metaObject, "createTime", Date :: new, Date.class); // 起始版本 3.3.3(推荐)
+        strictInsertFill(metaObject, "createTime", Date::new, Date.class); // 起始版本 3.3.3(推荐)
 
-        this.strictInsertFill(metaObject, "updateTime", Date :: new, Date.class); // 起始版本 3.3.3(推荐)
+        strictInsertFill(metaObject, "updateTime", Date::new, Date.class); // 起始版本 3.3.3(推荐)
 
-        this.strictInsertFill(metaObject, "delFlag", () -> 0, Integer.class); // 起始版本 3.3.3(推荐)
+        strictInsertFill(metaObject, "delFlag", () -> 0, Integer.class); // 起始版本 3.3.3(推荐)
 
     }
 
     @Override
     public void updateFill(MetaObject metaObject) {
         log.info("start update fill ....");
-        this.strictUpdateFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now()); // 起始版本 3.3.0(推荐)
-        // 或者
-        this.strictUpdateFill(metaObject, "updateTime", () -> LocalDateTime.now(), LocalDateTime.class); // 起始版本 3.3.3(推荐)
-        // 或者
-        this.fillStrategy(metaObject, "updateTime", LocalDateTime.now()); // 也可以使用(3.3.0 该方法有bug)
+        strictUpdateFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now()); // 起始版本 3.3.0(推荐)
+
     }
 
 
