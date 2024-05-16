15e3b32ce5889343f717f253555b8a09ef669a87
风吹半梦半醒 <2422016922@qq.com>
2024-05-16 15:58:41 +0800

    用户登录Resp

---
+++ admin/src/main/java/com_awake_CloserLink/Dto/Respons/UserLoginRespDTO.java
@@ -0,0 +1,16 @@
+package com_awake_CloserLink.Dto.Respons;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Author 清醒
+ * @Date 2024/5/15 22:51
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserLoginRespDTO {
+    private String token;
+}
