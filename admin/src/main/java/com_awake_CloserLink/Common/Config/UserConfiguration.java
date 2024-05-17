da7d0fae428627c22f7bdbea426b075e85f7037e
风吹半梦半醒 <2422016922@qq.com>
2024-05-17 16:40:19 +0800

    过滤器(用户配置自动装配)

---
+++ admin/src/main/java/com_awake_CloserLink/Common/Config/UserConfiguration.java
@@ -0,0 +1,30 @@
+package com_awake_CloserLink.Common.Config;
+
+/**
+ * @Author 清醒
+ * @Date 2024/5/16 22:32
+ */
+
+import com_awake_CloserLink.Common.Biz.UserTransmitFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 用户配置自动装配
+ */
+@Configuration
+public class UserConfiguration {
+
+    /**
+     * 用户信息传递过滤器
+     */
+    @Bean
+    public FilterRegistrationBean<UserTransmitFilter> globalUserTransmitFilter() {
+        FilterRegistrationBean<UserTransmitFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(new UserTransmitFilter());
+        registration.addUrlPatterns("/*");
+        registration.setOrder(0);
+        return registration;
+    }
+}
