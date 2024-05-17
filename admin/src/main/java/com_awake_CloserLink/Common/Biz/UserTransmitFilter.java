bb634ac9ff62b6adf6b8a81aed0c0fe1e239913f
风吹半梦半醒 <2422016922@qq.com>
2024-05-17 19:50:27 +0800

    过滤器放行用户登录路径

--- admin/src/main/java/com_awake_CloserLink/Common/Biz/UserTransmitFilter.java
+++ admin/src/main/java/com_awake_CloserLink/Common/Biz/UserTransmitFilter.java
@@ -9,28 +9,34 @@ import com.alibaba.fastjson2.JSON;
 import com_awake_CloserLink.Entitys.UserInfoDTO;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.io.IOException;
+import java.util.Objects;
 
 /**
  * 用户信息传输过滤器
  */
+@RequiredArgsConstructor
 public class UserTransmitFilter implements Filter {
-    @Autowired
-    private StringRedisTemplate stringRedisTemplate;
+
+    private final StringRedisTemplate stringRedisTemplate;
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
-        String username = httpServletRequest.getHeader("username");
-        String token = httpServletRequest.getHeader("token");
-        Object userInfo = stringRedisTemplate.opsForHash().get("login:" + username, token);
-        if (userInfo != null) {
-            UserInfoDTO userInfoDTO =
-                    JSON.parseObject(userInfo.toString(), UserInfoDTO.class);
-            UserContext.setUser(userInfoDTO);
+
+        String requestURI = httpServletRequest.getRequestURI();
+        if (!Objects.equals(requestURI, "/api/short-link/v1/user/login")) {
+            String username = httpServletRequest.getHeader("username");
+            String token = httpServletRequest.getHeader("token");
+            Object userInfo = stringRedisTemplate.opsForHash().get("login:" + username, token);
+            if (userInfo != null) {
+                UserInfoDTO userInfoDTO =
+                        JSON.parseObject(userInfo.toString(), UserInfoDTO.class);
+                UserContext.setUser(userInfoDTO);
+            }
         }
 
         try {
