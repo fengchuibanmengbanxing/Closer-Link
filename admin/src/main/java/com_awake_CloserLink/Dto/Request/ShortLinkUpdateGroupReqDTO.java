904099c88d64ce10680c484333c8d627bdeefe87
风吹半梦半醒 <2422016922@qq.com>
2024-05-17 16:39:13 +0800

    分组修改响应类

---
+++ admin/src/main/java/com_awake_CloserLink/Dto/Request/ShortLinkUpdateGroupReqDTO.java
@@ -0,0 +1,13 @@
+package com_awake_CloserLink.Dto.Request;
+
+import lombok.Data;
+
+/**
+ * @Author 清醒
+ * @Date 2024/5/17 16:35
+ */
+@Data
+public class ShortLinkUpdateGroupReqDTO {
+    private Integer gid;
+    private String groupName;
+}
