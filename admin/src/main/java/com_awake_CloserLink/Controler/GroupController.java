581a252ad41cd765cc0e4ae1df1d7de365aa779e
风吹半梦半醒 <2422016922@qq.com>
2024-05-17 16:36:47 +0800

    根据用户名获取短链接集合

--- admin/src/main/java/com_awake_CloserLink/Controler/GroupController.java
+++ admin/src/main/java/com_awake_CloserLink/Controler/GroupController.java
@@ -3,11 +3,15 @@ package com_awake_CloserLink.Controler;
 import com_awake_CloserLink.Common.Convention.result.Result;
 import com_awake_CloserLink.Common.Convention.result.Results;
 import com_awake_CloserLink.Dto.Request.ShortLinkSaveGroupReqDTO;
+import com_awake_CloserLink.Dto.Respons.ShortLinkGroupRespDTO;
 import com_awake_CloserLink.Service.GroupService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @Author 清醒
@@ -25,7 +29,17 @@ public class GroupController {
      */
     @PostMapping("/api/short-link/v1/group")
     public Result<Void> saveGroup(@RequestBody ShortLinkSaveGroupReqDTO shortLinkSaveGroupReqDTO){
-        groupService.saveGroup(shortLinkSaveGroupReqDTO.getName());
+        groupService.saveGroup(shortLinkSaveGroupReqDTO.getGroupName());
         return Results.success();
+    }
+    /**
+     * 返回短链接分组集合
+     * @param
+     * @return
+     */
+    @GetMapping("/api/short-link/v1/group")
+    public Result<List<ShortLinkGroupRespDTO>> listGroup(){
+        List<ShortLinkGroupRespDTO> list=groupService.listGroup();
+        return Results.success(list);
     }
 }
