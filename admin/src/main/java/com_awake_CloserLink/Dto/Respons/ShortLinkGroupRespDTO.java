01f203de9c061f339a48ce3da0865896b890cbdd
风吹半梦半醒 <2422016922@qq.com>
2024-05-17 16:38:43 +0800

    分组响应类

---
+++ admin/src/main/java/com_awake_CloserLink/Dto/Respons/ShortLinkGroupRespDTO.java
@@ -0,0 +1,30 @@
+package com_awake_CloserLink.Dto.Respons;
+
+import lombok.Data;
+
+/**
+ * @Author 清醒
+ * @Date 2024/5/16 21:50
+ */
+@Data
+public class ShortLinkGroupRespDTO {
+    /**
+     * 分组标识
+     */
+    private String gid;
+
+    /**
+     * 分组名称
+     */
+    private String name;
+
+    /**
+     * 创建分组用户名
+     */
+    private String username;
+
+    /**
+     * 分组排序
+     */
+    private String sortOrder;
+}
