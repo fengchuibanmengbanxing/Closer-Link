a76a07c9fb6c6443add7895ded1d6936195a4ab7
风吹半梦半醒 <2422016922@qq.com>
2024-05-17 16:38:09 +0800

    根据用户名获取短链接集合

--- admin/src/main/java/com_awake_CloserLink/Service/GroupService.java
+++ admin/src/main/java/com_awake_CloserLink/Service/GroupService.java
@@ -1,7 +1,10 @@
 package com_awake_CloserLink.Service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com_awake_CloserLink.Dto.Respons.ShortLinkGroupRespDTO;
 import com_awake_CloserLink.Entitys.GroupDO;
+
+import java.util.List;
 
 /**
  * @Author 清醒
@@ -12,5 +15,13 @@ public interface GroupService extends IService<GroupDO> {
      * 保存短链接组
      * @param username
      */
-    void saveGroup(String username);
+    void saveGroup(String groupName);
+
+    /**
+     *返回短链接分组集合
+     * @return
+     */
+    List<ShortLinkGroupRespDTO> listGroup();
+
+
 }
--- admin/src/main/java/com_awake_CloserLink/Service/impl/GroupServiceImpl.java
+++ admin/src/main/java/com_awake_CloserLink/Service/impl/GroupServiceImpl.java
@@ -1,13 +1,18 @@
 package com_awake_CloserLink.Service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com_awake_CloserLink.Common.Biz.UserContext;
+import com_awake_CloserLink.Dto.Respons.ShortLinkGroupRespDTO;
 import com_awake_CloserLink.Entitys.GroupDO;
 import com_awake_CloserLink.Mapper.GroupMapper;
 import com_awake_CloserLink.Service.GroupService;
 import com_awake_CloserLink.Utils.RandomUtil;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @Author 清醒
@@ -19,10 +24,10 @@ public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implemen
     /**
      * 保存短链接组
      *
-     * @param username
+     * @param groupName
      */
     @Override
-    public void saveGroup(String username) {
+    public void saveGroup(String groupName) {
         /**
          * 生成随机六位数据组id
          */
@@ -34,15 +39,29 @@ public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implemen
         }
         GroupDO groupDO = GroupDO.builder()
                 .gid(gid)
-                .name(username).build();
+                .name(UserContext.getUsername())
+                .sortOrder(0)
+                .build();
         baseMapper.insert(groupDO);
+    }
+
+    @Override
+    public List<ShortLinkGroupRespDTO> listGroup() {
+        LambdaQueryWrapper<GroupDO> queryWrapper = Wrappers.lambdaQuery(GroupDO.class)
+                .eq(GroupDO::getDelFlag,0)
+                .eq(GroupDO::getUsername, UserContext.getUsername())
+                .orderByDesc(GroupDO::getSortOrder,GroupDO::getUpdateTime);
+        List<GroupDO> groupDOS = baseMapper.selectList(queryWrapper);
+        return BeanUtil.copyToList(groupDOS, ShortLinkGroupRespDTO.class);
     }
 
 
     //判断是否生成的gid是否重复
     private boolean hasGid(String gid) {
+        //TODO 获取用户名
         LambdaQueryWrapper<GroupDO> queryWrapper = Wrappers.lambdaQuery(GroupDO.class)
-                .eq(GroupDO::getGid, gid);
+                .eq(GroupDO::getGid, gid)
+                .eq(GroupDO::getName,UserContext.getUsername());
         return baseMapper.selectOne(queryWrapper) != null;
     }
 
