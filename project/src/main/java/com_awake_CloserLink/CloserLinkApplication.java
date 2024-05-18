a898359a8bd43dd1d0d522016595484f2e814251
风吹半梦半醒 <2422016922@qq.com>
2024-05-18 11:35:54 +0800

    启动类

---
+++ project/src/main/java/com_awake_CloserLink/CloserLinkApplication.java
@@ -0,0 +1,16 @@
+package com_awake_CloserLink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author 清醒
+ * @Date 2024/5/18 11:11
+ */
+@SpringBootApplication
+public class CloserLinkApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CloserLinkApplication.class, args);
+    }
+
+}
