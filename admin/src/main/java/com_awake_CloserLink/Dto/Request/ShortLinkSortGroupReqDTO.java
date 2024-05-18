a8db8b7d7c6775fcbadb28d8805b289eaf4323be
风吹半梦半醒 <2422016922@qq.com>
2024-05-18 11:31:17 +0800

    短链接分组排序请求类

---
+++ admin/src/main/java/com_awake_CloserLink/Dto/Request/ShortLinkSortGroupReqDTO.java
@@ -0,0 +1,13 @@
+package com_awake_CloserLink.Dto.Request;
+
+import lombok.Data;
+
+/**
+ * @Author 清醒
+ * @Date 2024/5/17 22:03
+ */
+@Data
+public class ShortLinkSortGroupReqDTO {
+    private String gid;
+    private Integer sortOrder;
+}
