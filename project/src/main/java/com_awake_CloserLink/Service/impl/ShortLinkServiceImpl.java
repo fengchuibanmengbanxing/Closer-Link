9fbd6cfd7b61c42da6aaf688aefb4c2e5f0be8f6
风吹半梦半醒 <2422016922@qq.com>
2024-05-18 15:41:27 +0800

    使用布隆过滤器去除重复创建短链接

--- project/src/main/java/com_awake_CloserLink/Service/impl/ShortLinkServiceImpl.java
+++ project/src/main/java/com_awake_CloserLink/Service/impl/ShortLinkServiceImpl.java
@@ -1,13 +1,19 @@
 package com_awake_CloserLink.Service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com_awake_CloserLink.Common.Convention.Exception.ServiceException;
 import com_awake_CloserLink.Dto.Req.ShortLinkCreatReqDTO;
 import com_awake_CloserLink.Dto.Resp.ShortLinkCreatRespDTO;
 import com_awake_CloserLink.Entitys.LinkDO;
 import com_awake_CloserLink.Mapper.ShortLinkMapper;
 import com_awake_CloserLink.Service.ShortLinkService;
 import com_awake_CloserLink.Utils.HashUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RBloomFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -15,34 +21,67 @@ import org.springframework.stereotype.Service;
  * @Date 2024/5/18 9:53
  */
 @Service
+@Slf4j
 public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, LinkDO> implements ShortLinkService {
+    //创建短链接布隆过滤器
+    @Autowired
+    private RBloomFilter<String> shortLinkCreatCachePenetrationBloomFilter;
+
     //创建短链接
     @Override
     public ShortLinkCreatRespDTO creatShortLink(ShortLinkCreatReqDTO shortLinkCreatReqDTO) {
         //短链接后缀
         String generateSuffix = generateSuffix(shortLinkCreatReqDTO);
         LinkDO shortLinkDO = BeanUtil.toBean(shortLinkCreatReqDTO, LinkDO.class);
+        String fullShortUrl = shortLinkDO.getDomain() + "/" + generateSuffix;
         //设置完整短链接
-        shortLinkDO.setFullShortUrl(shortLinkDO.getDomain()+"/"+generateSuffix);
+        shortLinkDO.setFullShortUrl(fullShortUrl);
         shortLinkDO.setEnableStatus(1);
         shortLinkDO.setShortUri(generateSuffix);
-        baseMapper.insert(shortLinkDO);
+        //防止数据库写入后但是布隆过滤器未添加短链接导致下次请求短链接缓存穿透
+        try {
+            baseMapper.insert(shortLinkDO);
+        } catch (Exception e) {
+            LambdaQueryWrapper<LinkDO> queryWrapper = Wrappers.lambdaQuery(LinkDO.class)
+                    .eq(LinkDO::getFullShortUrl, fullShortUrl);
+            LinkDO linkDO = baseMapper.selectOne(queryWrapper);
+            if (linkDO != null) {
+                log.error("数据库写入失败，请检查！");
+                throw new ServiceException("短链接生成重复！");
+            }
+        }
+        shortLinkCreatCachePenetrationBloomFilter.add(fullShortUrl);
 
         //返回响应类RespDTO
+        //流式创建对象
         return ShortLinkCreatRespDTO.builder()
                 .gid(shortLinkCreatReqDTO.getGid())
                 .originUrl(shortLinkCreatReqDTO.getOriginUrl())
                 .fullShortUrl(shortLinkDO.getFullShortUrl())
                 .build();
+
     }
 
 
     //工具类实现短链接
     private String generateSuffix(ShortLinkCreatReqDTO shortLinkCreatReqDTO) {
+        int creatgenerateSuffix = 0;
         String originUrl = shortLinkCreatReqDTO.getOriginUrl();
-        String s = HashUtil.hashToBase62(originUrl);
-        return s;
+        String shortUri;
+        while (true) {
+            if (creatgenerateSuffix > 10) {
+                throw new ServiceException("多次创建短链接，请稍后重试！");
+            }
+            //相同网址可能由于毫秒数不同可以创建多个链接
+            originUrl += System.currentTimeMillis();
+            //该工具类只要内容不更改生成的数据仍是一样
+            shortUri = HashUtil.hashToBase62(originUrl);
+            //布隆过滤器不包含一定就不包含
+            if (!shortLinkCreatCachePenetrationBloomFilter.contains(shortLinkCreatReqDTO.getDomain() + "/" + shortUri)) {
+                break;
+            }
+            creatgenerateSuffix++;
+        }
+        return shortUri;
     }
-
-
 }
