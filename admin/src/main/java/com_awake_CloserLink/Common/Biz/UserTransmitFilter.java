7daa90b28df776e157885780fcc75ab638b509c7
风吹半梦半醒 <2422016922@qq.com>
2024-05-18 21:00:25 +0800

    过滤器修改

--- admin/src/main/java/com_awake_CloserLink/Common/Biz/UserTransmitFilter.java
+++ admin/src/main/java/com_awake_CloserLink/Common/Biz/UserTransmitFilter.java
@@ -5,7 +5,10 @@ package com_awake_CloserLink.Common.Biz;
  * @Date 2024/5/16 22:30
  */
 
+import cn.hutool.core.util.StrUtil;
 import com.alibaba.fastjson2.JSON;
+import com.google.common.collect.Lists;
+import com_awake_CloserLink.Common.Convention.Exception.ClientException;
 import com_awake_CloserLink.Entitys.UserInfoDTO;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,7 +16,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Objects;
+
+import static com_awake_CloserLink.Common.Enums.UserErrorCodeEnum.IDEMPOTENT_TOKEN_NULL_ERROR;
 
 /**
  * 用户信息传输过滤器
@@ -23,19 +29,39 @@ public class UserTransmitFilter implements Filter {
 
     private final StringRedisTemplate stringRedisTemplate;
 
+    private static final List<String> IGNORE_URI = Lists.newArrayList(
+            "/api/short-link/admin/v1/user/login",
+            "/api/short-link/admin/v1/actual/has-username"
+    );
+
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
 
         String requestURI = httpServletRequest.getRequestURI();
-        if (!Objects.equals(requestURI, "/api/short-link/v1/user/login")) {
-            String username = httpServletRequest.getHeader("username");
-            String token = httpServletRequest.getHeader("token");
-            Object userInfo = stringRedisTemplate.opsForHash().get("login:" + username, token);
-            if (userInfo != null) {
+        if (!IGNORE_URI.contains(requestURI)) {
+            String method = httpServletRequest.getMethod();
+            if (!("POST".equals(method)) && Objects.equals(requestURI, "/api/short-link/admin/v1/user")) {
+                String username = httpServletRequest.getHeader("username");
+                String token = httpServletRequest.getHeader("token");
+                if (!StrUtil.isAllNotBlank(username, token)) {
+                    throw new ClientException(IDEMPOTENT_TOKEN_NULL_ERROR);
+                }
+                Object userInfo = null;
+                try {
+                    userInfo = stringRedisTemplate.opsForHash().get("login:" + username, token);
+                    if (userInfo==null){
+                        throw new ClientException(IDEMPOTENT_TOKEN_NULL_ERROR);
+                    }
+                } catch (Exception e) {
+                    throw new ClientException(IDEMPOTENT_TOKEN_NULL_ERROR);
+                }
+
+
                 UserInfoDTO userInfoDTO =
                         JSON.parseObject(userInfo.toString(), UserInfoDTO.class);
                 UserContext.setUser(userInfoDTO);
+
             }
         }
 
