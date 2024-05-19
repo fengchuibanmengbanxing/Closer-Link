7f1e264681b879cb4c022f28358b783a816bb10f
风吹半梦半醒 <2422016922@qq.com>
2024-05-19 20:42:41 +0800

    日期格式化

--- project/src/main/java/com_awake_CloserLink/Dto/Req/ShortLinkCreatReqDTO.java
+++ project/src/main/java/com_awake_CloserLink/Dto/Req/ShortLinkCreatReqDTO.java
@@ -1,5 +1,6 @@
 package com_awake_CloserLink.Dto.Req;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.util.Date;
@@ -41,6 +42,7 @@ public class ShortLinkCreatReqDTO {
     /**
      * 有效期
      */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date validDate;
 
     /**
