8764eceeb2beeb71f2a95e001008b7a4a2a12841
风吹半梦半醒 <2422016922@qq.com>
2024-05-20 15:56:44 +0800

    redis缓存key

---
+++ project/src/main/java/com_awake_CloserLink/Common/Constant/RedisKeyConstant.java
@@ -0,0 +1,17 @@
+package com_awake_CloserLink.Common.Constant;
+
+/**
+ * @Author 清醒
+ * @Date 2024/5/20 15:12
+ */
+public class RedisKeyConstant {
+    /**
+     * 短链接reidskey
+     */
+    public static final String GOTO_SHORT_LINK_KEY = "goto_short_link_%s";
+
+    /**
+     * 短链接reidskey
+     */
+    public static final String LOCK_GOTO_SHORT_LINK_KEY = "lock_goto_short_link_%s";
+}
