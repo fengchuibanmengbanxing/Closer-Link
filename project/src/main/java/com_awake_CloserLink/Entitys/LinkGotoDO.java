463bc8dbe1d96b136d8baae35b86f01dafbdd489
风吹半梦半醒 <2422016922@qq.com>
2024-05-20 15:56:59 +0800

    分库分表

---
+++ project/src/main/java/com_awake_CloserLink/Entitys/LinkGotoDO.java
@@ -0,0 +1,22 @@
+package com_awake_CloserLink.Entitys;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Author 清醒
+ * @Date 2024/5/20 12:01
+ */
+@Data
+@TableName("t_link_goto")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LinkGotoDO {
+    private Long id;
+    private String gid;
+    private String fullShortUrl;
+}
