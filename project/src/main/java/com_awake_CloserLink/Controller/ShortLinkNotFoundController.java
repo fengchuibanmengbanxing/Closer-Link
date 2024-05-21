659253f9866bc0aada4ef8928a21dcea4ecb87fe
风吹半梦半醒 <2422016922@qq.com>
2024-05-21 12:59:47 +0800

    错误提示用户界面设置

---
+++ project/src/main/java/com_awake_CloserLink/Controller/ShortLinkNotFoundController.java
@@ -0,0 +1,24 @@
+package com_awake_CloserLink.Controller;
+
+/**
+ * @Author 清醒
+ * @Date 2024/5/21 10:45
+ */
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 短链接不存在跳转控制器
+ */
+@Controller
+public class ShortLinkNotFoundController {
+
+    /**
+     * 短链接不存在跳转页面
+     */
+    @RequestMapping("/page/notfound")
+    public String notfound() {
+        return "notfound";
+    }
+}
