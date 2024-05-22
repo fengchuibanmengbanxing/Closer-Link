43ffe1abfad476ac9e1e8b953b35674157ff0a1e
风吹半梦半醒 <2422016922@qq.com>
2024-05-22 20:17:44 +0800

    回收站删除响应类

---
+++ admin/src/main/java/com_awake_CloserLink/Remote/Req/RecycleBinRemoveReqDTO.java
@@ -0,0 +1,28 @@
+package com_awake_CloserLink.Remote.Req;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Author 清醒
+ * @Date 2024/5/22 19:52
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class RecycleBinRemoveReqDTO {
+
+    /**
+     * 分组标识
+     */
+    private String gid;
+    /**
+     * 完整链
+     */
+    private String fullShortUrl;
+
+}
