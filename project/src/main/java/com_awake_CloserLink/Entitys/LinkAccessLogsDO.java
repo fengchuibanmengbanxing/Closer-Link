df1ae320616e02dbe88b0479a48439e0afe07749
风吹半梦半醒 <2422016922@qq.com>
2024-05-24 20:34:08 +0800

    短链接监控高频访问ip实体类

---
+++ project/src/main/java/com_awake_CloserLink/Entitys/LinkAccessLogsDO.java
@@ -0,0 +1,53 @@
+package com_awake_CloserLink.Entitys;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Author 清醒
+ * @Date 2024/5/24 20:19
+ */
+@Data
+@TableName("t_link_access_logs")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LinkAccessLogsDO {
+    /**
+     * ID
+     */
+    private Long id;
+
+    /**
+     * 完整短链接
+     */
+    private String fullShortUrl;
+
+    /**
+     * 分组标识
+     */
+    private String gid;
+
+    /**
+     * 用户信息
+     */
+    private String user;
+
+    /**
+     * 浏览器
+     */
+    private String browser;
+
+    /**
+     * 操作系统
+     */
+    private String os;
+
+    /**
+     * IP
+     */
+    private String ip;
+}
