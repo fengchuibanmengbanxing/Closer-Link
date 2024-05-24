c2b24f7eb19bcfea48f81184d88775638def2c00
风吹半梦半醒 <2422016922@qq.com>
2024-05-24 15:27:40 +0800

    获取ip工具类

--- project/src/main/java/com_awake_CloserLink/Utils/LinkUtil.java
+++ project/src/main/java/com_awake_CloserLink/Utils/LinkUtil.java
@@ -7,6 +7,7 @@ package com_awake_CloserLink.Utils;
 
 import cn.hutool.core.date.DateUnit;
 import cn.hutool.core.date.DateUtil;
+import jakarta.servlet.http.HttpServletRequest;
 
 import java.util.Date;
 import java.util.Optional;
@@ -26,5 +27,26 @@ public class LinkUtil {
         return Optional.ofNullable(valid)
                 .map(v -> DateUtil.between(new Date(),v, DateUnit.MS))
                 .orElse(2419200000L);
+    }
+
+    /**
+     * 获取ip
+     * @param request
+     * @return
+     */
+    public static String getIp(HttpServletRequest request) {
+        String ipAddress = request.getHeader("X-Forwarded-For");
+
+        if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("Proxy-Client-IP");
+        }
+        if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getRemoteAddr();
+        }
+
+        return ipAddress;
     }
 }
