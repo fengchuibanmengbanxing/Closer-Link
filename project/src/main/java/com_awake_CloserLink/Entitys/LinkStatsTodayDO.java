b1df112d8caad759b21d757304d9c6a89013d1ba
风吹半梦半醒 <2422016922@qq.com>
2024-05-28 14:43:06 +0800

    fix:今日数据监控实体类

---
+++ project/src/main/java/com_awake_CloserLink/Entitys/LinkStatsTodayDO.java
@@ -0,0 +1,58 @@
+package com_awake_CloserLink.Entitys;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com_awake_CloserLink.Common.Database.BaseDO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * @Author 清醒
+ * @Date 2024/5/27 20:47
+ */
+@Data
+@TableName("t_link_stats_today")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LinkStatsTodayDO extends BaseDO {
+    /**
+     * ID
+     */
+    private Long id;
+
+    /**
+     * 分组标识
+     */
+    private String gid;
+
+    /**
+     * 短链接
+     */
+    private String fullShortUrl;
+
+    /**
+     * 日期
+     */
+    private Date date;
+
+    /**
+     * PV
+     */
+    private Integer todayPv;
+
+    /**
+     * UV
+     */
+    private Integer todayUv;
+
+    /**
+     * IP
+     */
+    private Integer todayUip;
+
+
+}
