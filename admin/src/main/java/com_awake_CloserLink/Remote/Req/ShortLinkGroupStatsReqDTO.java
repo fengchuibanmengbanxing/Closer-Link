b34f38dae0b026764c37306d11b1f2a32e3fc55d
风吹半梦半醒 <2422016922@qq.com>
2024-05-28 18:55:47 +0800

    feature:分组短链接访客记录请求类

---
+++ admin/src/main/java/com_awake_CloserLink/Remote/Req/ShortLinkGroupStatsReqDTO.java
@@ -0,0 +1,26 @@
+package com_awake_CloserLink.Remote.Req;
+
+import lombok.Data;
+
+/**
+ * @Author 清醒
+ * @Date 2024/5/28 14:59
+ */
+@Data
+public class ShortLinkGroupStatsReqDTO {
+
+    /**
+     * 分组标识
+     */
+    private String gid;
+
+    /**
+     * 开始日期
+     */
+    private String startDate;
+
+    /**
+     * 结束日期
+     */
+    private String endDate;
+}
