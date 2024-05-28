9765ed6e02af4d2fa2f46b57552f2dd55afae834
风吹半梦半醒 <2422016922@qq.com>
2024-05-28 14:42:04 +0800

    fix:修改单一短链接访客记录

--- project/src/main/java/com_awake_CloserLink/Service/LinkStatsService.java
+++ project/src/main/java/com_awake_CloserLink/Service/LinkStatsService.java
@@ -1,6 +1,9 @@
 package com_awake_CloserLink.Service;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com_awake_CloserLink.Dto.Req.ShortLinkStatsAccessRecordReqDTO;
 import com_awake_CloserLink.Dto.Req.ShortLinkStatsReqDTO;
+import com_awake_CloserLink.Dto.Resp.ShortLinkStatsAccessRecordRespDTO;
 import com_awake_CloserLink.Dto.Resp.ShortLinkStatsRespDTO;
 
 /**
@@ -15,4 +18,10 @@ public interface LinkStatsService {
      */
     ShortLinkStatsRespDTO getOneStats(ShortLinkStatsReqDTO shortLinkStatsReqDTO);
 
+    /**
+     * 获取单个短链接访客访问记录
+     * @param shortLinkStatsAccessRecordReqDTO
+     * @return
+     */
+    IPage<ShortLinkStatsAccessRecordRespDTO> getAccessRecordStats(ShortLinkStatsAccessRecordReqDTO shortLinkStatsAccessRecordReqDTO);
 }
