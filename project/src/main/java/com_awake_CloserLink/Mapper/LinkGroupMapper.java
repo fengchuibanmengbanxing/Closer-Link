d177dae33cdf7a12ad57dce3a46bfb6b7e77119a
风吹半梦半醒 <2422016922@qq.com>
2024-05-28 18:54:08 +0800

    feature:分组mapper

---
+++ project/src/main/java/com_awake_CloserLink/Mapper/LinkGroupMapper.java
@@ -0,0 +1,13 @@
+package com_awake_CloserLink.Mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com_awake_CloserLink.Entitys.GroupDO;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @Author 清醒
+ * @Date 2024/5/28 15:23
+ */
+@Mapper
+public interface LinkGroupMapper extends BaseMapper<GroupDO> {
+}
