e66da9966b409e8bec5b33fd08a179f7e19e6a78
风吹半梦半醒 <2422016922@qq.com>
2024-05-29 20:45:00 +0800

    feature:白名单配置文件

--- project/src/main/java/com_awake_CloserLink/CloserLinkApplication.java
+++ project/src/main/java/com_awake_CloserLink/CloserLinkApplication.java
@@ -1,13 +1,16 @@
 package com_awake_CloserLink;
 
+import com_awake_CloserLink.Common.Config.GotoDomainWhiteListConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 /**
  * @Author 清醒
  * @Date 2024/5/18 11:11
  */
 @SpringBootApplication
+@EnableConfigurationProperties(value = {GotoDomainWhiteListConfiguration.class})
 public class CloserLinkApplication {
     public static void main(String[] args) {
         SpringApplication.run(CloserLinkApplication.class, args);
