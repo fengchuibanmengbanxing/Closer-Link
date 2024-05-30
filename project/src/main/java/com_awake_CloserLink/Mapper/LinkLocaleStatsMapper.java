ff0a6b0c551d259fc291ff160a6f0cf4acc498f3
风吹半梦半醒 <2422016922@qq.com>
2024-05-30 19:21:58 +0800

    fix:修改类名

--- project/src/main/java/com_awake_CloserLink/Mapper/LinkLocaleStatsMapper.java
+++ project/src/main/java/com_awake_CloserLink/Mapper/LinkLocaleStatsMapper.java
@@ -21,7 +21,7 @@ public interface LinkLocaleStatsMapper {
             "t_link_locale_stats (gid,full_short_url,date,cnt,province,city,adcode,country,create_time,update_time,del_flag) " +
             "VALUES(#{linkLocaleStats.gid}, #{linkLocaleStats.fullShortUrl}, #{linkLocaleStats.date}, #{linkLocaleStats.cnt}, #{linkLocaleStats.province}, #{linkLocaleStats.city}, #{linkLocaleStats.adcode}, #{linkLocaleStats.country}, NOW(), NOW(), 0) " +
             "ON DUPLICATE KEY UPDATE cnt = cnt + #{linkLocaleStats.cnt}")
-    void shortLinkStatsIp(@Param("linkLocaleStats") LinkLocaleStatsDO linkLocaleStatsDO);
+    void shortLinkStatsLocale(@Param("linkLocaleStats") LinkLocaleStatsDO linkLocaleStatsDO);
 
 
     /**
