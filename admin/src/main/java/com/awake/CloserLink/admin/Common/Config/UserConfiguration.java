6ebebe4e92e0b35715009af9310313f1b63ad7eb
风吹半梦半醒 <2422016922@qq.com>
2024-06-02 20:59:48 +0800

    optimize:用户信息传递过滤器

--- admin/src/main/java/com/awake/CloserLink/admin/Common/Config/UserConfiguration.java
+++ admin/src/main/java/com/awake/CloserLink/admin/Common/Config/UserConfiguration.java
@@ -9,7 +9,6 @@ import com.awake.CloserLink.admin.Common.Biz.UserTransmitFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.core.StringRedisTemplate;
 
 /**
  * 用户配置自动装配
@@ -20,10 +19,11 @@ public class UserConfiguration {
     /**
      * 用户信息传递过滤器
      */
+
     @Bean
-    public FilterRegistrationBean<UserTransmitFilter> globalUserTransmitFilter(StringRedisTemplate stringRedisTemplate) {
+    public FilterRegistrationBean<UserTransmitFilter> globalUserTransmitFilter() {
         FilterRegistrationBean<UserTransmitFilter> registration = new FilterRegistrationBean<>();
-        registration.setFilter(new UserTransmitFilter(stringRedisTemplate));
+        registration.setFilter(new UserTransmitFilter());
         registration.addUrlPatterns("/*");
         registration.setOrder(0);
         return registration;
